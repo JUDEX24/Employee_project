0eafd9d4a4deb30138491cb6a6f15c54c658b551
JUDEX24 <cjude95@gmail.com>
2023-02-20 12:02:00 +0200

    Responsiveness and routing

--- src/App.js
+++ src/App.js
@@ -1,79 +1,19 @@
 import "./index.css";
-import { useState } from "react";
-import Employee from "./components/Employee";
-import AddEmployee from "./components/AddEmployee";
-import { v4 as uuidv4 } from "uuid";
+import Header from "./components/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import EmployeesPage from "./pages/EmployeesPage";
+import CustomersPage from "./pages/CustomersPage";
 
 function App() {
-  const [employees, setEmployees] = useState([
-    {
-      id: 1,
-      name: "Jay",
-      role: "Developer",
-      img: "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg",
-    },
-    {
-      id: 2,
-      name: "Jane",
-      role: "Designer",
-      img: "https://images.pexels.com/photos/1239288/pexels-photo-1239288.jpeg",
-    },
-    {
-      id: 3,
-      name: "George",
-      role: "Software Engineer",
-      img: "https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg",
-    },
-    {
-      id: 4,
-      name: "Lisa",
-      role: "Accountant",
-      img: "https://images.pexels.com/photos/3586798/pexels-photo-3586798.jpeg",
-    },
-  ]);
-
-  const updateEmployee = (id, newName, newRole) => {
-    const updatedEmployees = employees.map((employee) => {
-      if (employee.id === id) {
-        // spread employee, edit and return it
-        return { ...employee, name: newName, role: newRole };
-      }
-      return employee;
-    });
-
-    setEmployees(updatedEmployees);
-  };
-
-  const addEmployee = (name, role, img) => {
-    const newEmployee = { id: uuidv4(), name: name, role: role, img: img };
-    setEmployees([...employees, newEmployee]);
-  };
-
-  const showEmployees = true;
   return (
-    <div className="App">
-      {showEmployees ? (
-        <>
-          <div className="flex flex-wrap justify-center">
-            {employees.map((employee) => {
-              return (
-                <Employee
-                  key={employee.id}
-                  id={employee.id}
-                  name={employee.name}
-                  role={employee.role}
-                  img={employee.img}
-                  updateEmployee={updateEmployee}
-                />
-              );
-            })}
-          </div>
-          <AddEmployee addEmployee={addEmployee} />
-        </>
-      ) : (
-        <p>You cannot see the employees</p>
-      )}
-    </div>
+    <BrowserRouter>
+      <Header>
+        <Routes>
+          <Route path="/" element={<EmployeesPage />} />
+          <Route path="/customers" element={<CustomersPage />} />
+        </Routes>
+      </Header>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/CustomersPage.js
@@ -0,0 +1,5 @@
+const CustomersPage = () => {
+  return <h1>Hello Customer</h1>;
+};
+
+export default CustomersPage;
